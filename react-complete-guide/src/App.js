e1f74283224e2e149eff460dd2e1afb6a150ed86
unknown <rolvinss@gmail.com>
2017-11-21 10:15:04 +0530

    importing to github

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return React.createElement('div', null, React.createElement('h1', null, 'does this work now..'),'Hi, I\'m a React App!!! ' )
+  }
+}
+
+export default App;
